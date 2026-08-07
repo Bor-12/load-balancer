8731c0fd2161558254e4d87d660d454a57c4e0ec
Borja <borjagonzare@gmail.com>
2026-08-07 18:03:36 +0200

    feat: add basic reverse proxy

--- cmd/cloudbalancer/main.go
+++ cmd/cloudbalancer/main.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
+	"os"
 
-	"github.com/Bor-12/load-balancer/internal/app"
+	"github.com/Bor-12/load-balancer/internal/proxy"
 )
 
 func main() {
-	slog.Info(app.StartupMessage())
+	logger := slog.Default()
+	backendURL := os.Getenv("BACKEND_URL")
+
+	reverseProxy, err := proxy.New(backendURL, logger)
+	if err != nil {
+		logger.Error("invalid backend configuration", "error", err)
+		os.Exit(1)
+	}
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: reverseProxy,
+	}
+
+	logger.Info("CloudBalancer listening", "address", server.Addr, "backend_url", backendURL)
+
+	if err := server.ListenAndServe(); err != nil {
+		logger.Error("server stopped", "error", err)
+		os.Exit(1)
+	}
 }
